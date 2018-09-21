d67eb8cebe69a7d46851fad5233c85ceea4519ec
Dustin Lang <dstndstn@gmail.com>
2018-09-21 14:35:26 -0400

    add tests; add -x, x-y

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using Test
 using Example2018
 
 @show Polynomial([1,2,3])
@@ -6,3 +7,13 @@ using Example2018
 @show 3 * Polynomial([1,2,4,42])
 @show deriv(Polynomial([1,1,1,1,1]))
 @show deriv(Polynomial([0,0,1,0,0]))
+
+
+
+
+@test 1 == 1
+
+z = Polynomial([0])
+p = Polynomial([1,2,3])
+
+@test p+z == p
